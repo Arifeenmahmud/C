ac70aebc0c297d3347c5ddcf04ca3db1baa806a0
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2021-01-05 23:09:01 +0600

    array  Value adding

--- ForC/main.c
+++ ForC/main.c
@@ -8,9 +8,42 @@
 
 #include <stdio.h>
 
+
+//void print_array(int arr[], int size);
+
+void print_array(int arr[], int size)
+{
+int i;
+for (i=0; i<size; i++)
+printf("\t %d", arr[i]);
+}
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    int numbs[5];
+
+    int even_nums[5] = {2,4,6,8,10};
+    int odd_nums[5] = {1,3,5,7};
+    int childrens_age[] = {4,5,6};
+    int zero_init[5] = {0};
+    printf("\nStart\n");
+    printf("\nEven Number:\n");
+    print_array(even_nums,5);
+
+
+    printf("\n Childrens age:\n");
+    print_array(childrens_age,5);
     
-    printf("Hello, World!\n");
+    printf("\nOdd Number:\n");
+    print_array(odd_nums,5);
+    
+    printf("\n Zero Init:\n");
+    print_array(zero_init,5);
+    
+    
     return 0;
 }
+
+
+
