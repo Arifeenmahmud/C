f791822abef34263166ad3b00f1b46e670ac0c80
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2021-01-10 02:25:30 +0600

    2D array

--- TwoDimentionalArray/main.c
+++ TwoDimentionalArray/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 //
 //  main.c
 //  TwoDimentionalArray
@@ -6,10 +9,24 @@
 //  Copyright © 2021 Arifeen Mahmud. All rights reserved.
 //
 
-#include <stdio.h>
+int main(){
+int x[4][3];
+int i, j;
+for(i=0;i<4;i++) {
+    for(j=0;j<3;j++){
+//x[i][j] = i*j;
+    x[i][j] = i * j;
+    }
+    }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+
+    printf("output of 2D array:\n");
+    for(i=0;i<4;i++){
+    for(j=0;j<3;j++){
+    printf("%4d",x[i][j]);
+    }
+        printf("\n");
+    }
+    printf("\n");
+return 0;
 }
