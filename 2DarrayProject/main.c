9e800e043aac1992db78e18f685e594c1b3e403a
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2021-01-10 23:31:11 +0600

    Initial Commit

---
+++ 2DarrayProject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2DarrayProject
+//
+//  Created by Arifeen Mahmud on 10/1/21.
+//  Copyright © 2021 Arifeen Mahmud. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
