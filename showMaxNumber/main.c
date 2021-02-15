3f21d5b4ebb1035bc0c583ebce160a5fe35f6782
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-02-15 16:24:26 +0600

    add a Comment.

--- showMaxNumber/main.c
+++ showMaxNumber/main.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-
+//Show the max Number amongs 3 Inputs Numbers...
  int a, b, c, max;
  printf("Enter 3 Number \n");
  scanf("%d%d%d", &a,&b,&c);
