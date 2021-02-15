ec471c67fc7f22fd0e43f52ce5ae7ac8b4e6f678
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2021-02-15 15:55:06 +0600

    Show Maximum Number.

--- showMaxNumber/main.c
+++ showMaxNumber/main.c
@@ -9,7 +9,20 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+ int a, b, c, max;
+ printf("Enter 3 Number \n");
+ scanf("%d%d%d", &a,&b,&c);
+ if (a>b && a>c)
+ max = a;
+
+ else if (b>c)
+ max = b;
+ else
+ max = c;
+
+ printf("Maximum is %d\n", max);
+
+    
     return 0;
 }
