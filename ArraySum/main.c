290684b7248a445a254e20dd78fe9bd01e982108
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-04-08 16:15:05 +0600

    ArraySum

--- ArraySum/main.c
+++ ArraySum/main.c
@@ -9,17 +9,18 @@
 #include <stdio.h>
 
 int ArraySum(int MyArray[], int size) {
-int sum = 0;
+    int sum = 0;
 
-for(int i=0; i < size; i++) {
-    sum = sum + MyArray[i];
+    for(int i=0; i < size; i++) {
+        sum = sum + MyArray[i];
 
 }
 return sum;
 }
 
 int main() {
-int MyNumberArray [7] = {20,30,50,60,10,66,100};
-int sum_of_array = ArraySum(MyNumberArray,7);
-printf("Array Sum = %d \n", sum_of_array);
+    int MyNumberArray [7] = {20,30,50,60,10,66,100};
+    int sum_of_array = ArraySum(MyNumberArray,7);
+    printf("Array Sum = %d \n", sum_of_array);
+        
 }
